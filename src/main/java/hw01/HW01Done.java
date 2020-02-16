06c78e0469dd7e32dfb2e291b944777dbea352a2
mirzayev.mirzayev <mirzayev.mirzayev@gmail.com>
2020-02-16 23:54:58 +0400

    #3 Realized

--- src/main/java/hw01/HW01Done.java
+++ src/main/java/hw01/HW01Done.java
@@ -7,23 +7,25 @@ public class HW01Done {
         System.out.println("Let the game begin!");
         int a=(int)(Math.random()*(100+1));
         int count=0;
-
+        Scanner in=new Scanner(System.in);
+        System.out.print("Input your name, please: ");
+        String name=in.nextLine();
         while (count<=100) {
 
-            Scanner in=new Scanner(System.in);
-            int name=in.nextInt();
+            System.out.print("Input your number: ");
+            int number=in.nextInt();
 
-            if (name < a) {
+            if (number < a) {
                 System.out.println("Your number is too small. Please, try again.");count++;
             }
-            else if (name > a) {
+            else if (number > a) {
                 System.out.println("Your number is too big. Please, try again.");count++;
             }
             else if(count==100){
                 System.out.println("There is no attempt, you lose!");break;
             }
             else {
-                System.out.println("Congratulations, " + name);break;
+                System.out.println("Congratulations: " + name);break;
             }
 
         }
