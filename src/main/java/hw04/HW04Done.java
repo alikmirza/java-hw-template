7fb954e7b81520715030b67464096ac1aeeaf8aa
mirzayev.mirzayev <mirzayev.mirzayev@gmail.com>
2020-03-06 03:01:24 +0400

    #4 Updated

--- src/main/java/hw04/HW04Done.java
+++ src/main/java/hw04/HW04Done.java
@@ -4,7 +4,7 @@ public class HW04Done {
     public static void main(String[] args) {
 
         Pet pet=new Pet("Dog","Rock",5,30,new String[]{"eat","drink","sleep"});
-        String[][] schedule =new String[][]{{"Saturday", "Sunday"}, {"go to cinema", "hiking"}};
+        String[][] schedule =new String[][]{{"Sunday", "Monday"}, {"go to cinema", "hiking"}};
         Human father=new Human("Vito","Karleone",1970);
         Human mother=new Human("Jane","Karleone",1980);
         Human child=new Human("Michael","Karleone",1977,90,mother,father,schedule,pet);
